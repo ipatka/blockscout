74421f0fec600df373883fd8313d68479f37a5d6
Isaac Patka <ipatka@gmail.com>
2023-07-02 13:19:21 -0400

    foundry restart

---
+++ scripts/restartFoundry.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "***RESTART FOUNDRY***"
+
+if ! timeout 5 cast block; then
+    echo "Anvil NOK"
+    LATEST_BLOCK_HEX=`curl http://localhost:80/api\?module\=block\&action\=eth_block_number | jq .result | tr -d '"' | cut -c 3-` && \
+    LATEST_BLOCK=`echo $(( 16#$LATEST_BLOCK_HEX ))` && \
+    FORK_BLOCK=$(($LATEST_BLOCK+1)) && \
+    echo "Restarting At"
+    echo $FORK_BLOCK
+    FORK_BLOCK=$FORK_BLOCK docker compose -f $BLOCKSCOUT/docker-compose/services/docker-compose-anvil.yml up -d
+else
+    echo "Anvil OK"
+fi
+
+exit 0
