abd1ffd961992d612b2c16ca2ad75a86f94655ae
Isaac Patka <ipatka@gmail.com>
2023-07-04 03:14:44 -0400

    fix health job

--- scripts/restartFoundry.sh
+++ scripts/restartFoundry.sh
@@ -6,12 +6,14 @@ if ! timeout 5 cast block; then
     LATEST_BLOCK_HEX=`curl http://localhost:80/api\?module\=block\&action\=eth_block_number | jq .result | tr -d '"' | cut -c 3-` && \
     LATEST_BLOCK=`echo $(( 16#$LATEST_BLOCK_HEX ))` && \
     FORK_BLOCK=$(($LATEST_BLOCK+1)) && \
-    echo "Restarting At"
-    echo $FORK_BLOCK
-    FORK_BLOCK=$FORK_BLOCK docker compose -f $BLOCKSCOUT/docker-compose/docker-compose-anvil.yml up -d
+    echo "Restarting At $FORK_BLOCK"
+    
+    BLOCK_RANGES="$BASE_RANGE,$FORK_BLOCK..latest"
+    echo "With Range $BLOCK_RANGES"
+    FORK_BLOCK=$FORK_BLOCK BLOCK_RANGES=$BLOCK_RANGES docker compose -f $BLOCKSCOUT/docker-compose/docker-compose-anvil.yml up -d
     
     RPC_URL=http://localhost:80/rpc/
-
+    
     echo "Getting mainnet time..." && \
     BLOCKTIME=`cast block --rpc-url $MAINNET | grep "timestamp" | grep -Eo '[0-9]{10}'` && \
     echo $BLOCKTIME && \
