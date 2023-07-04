d0a11af193ca253526a34ba23dab92f14feb287d
Isaac Patka <ipatka@gmail.com>
2023-07-03 23:08:38 -0400

    fix restart script

--- scripts/restartFoundry.sh
+++ scripts/restartFoundry.sh
@@ -9,6 +9,16 @@ if ! timeout 5 cast block; then
     echo "Restarting At"
     echo $FORK_BLOCK
     FORK_BLOCK=$FORK_BLOCK docker compose -f $BLOCKSCOUT/docker-compose/docker-compose-anvil.yml up -d
+    
+    RPC_URL=http://localhost:80/rpc/
+
+    echo "Getting mainnet time..." && \
+    BLOCKTIME=`cast block --rpc-url $MAINNET | grep "timestamp" | grep -Eo '[0-9]{10}'` && \
+    echo $BLOCKTIME && \
+    echo "Getting fork time..." && \
+    FORKTIME=`cast block --rpc-url $RPC_URL | grep "timestamp" | grep -Eo '[0-9]{10}'` && \
+    echo $FORKTIME && \
+    cast rpc evm_setNextBlockTimestamp $BLOCKTIME --rpc-url $RPC_URL
 else
     echo "Anvil OK"
 fi
